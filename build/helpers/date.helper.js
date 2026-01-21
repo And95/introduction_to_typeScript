2f3d7af0e6a317bd687fd5db890351ec4a9ff858
André Gonzaga <andregonzaga95@gmail.com>
2026-01-21 05:33:20 -0300

    Delete build directory

--- build/helpers/date.helper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class DataHelper {
-    static obterDataAgora() {
-        return new Date();
-    }
-}
-exports.default = DataHelper;
