4e06671a63ddcda041be2bcdf9542d1b4be9fffc
André Gonzaga <andregonzaga95@gmail.com>
2026-01-21 04:04:18 -0300

    Add folder build again

---
+++ build/helpers/age.helper.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class IdadeHelper {
+    static isAdult(age) {
+        return age >= 18;
+    }
+}
+exports.default = IdadeHelper;
